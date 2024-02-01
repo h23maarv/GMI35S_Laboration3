c9200db6453a403372ed163d4215ee36c9c9453f
h23maarv <h23maarv@du.se>
2024-02-01 12:27:24 +0100

    Rättade till alla funktioner, la till användarhistorik

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
-import requests
 import modules
-import config
-
 menu = 0
-while menu != 4:
+while menu != 5:
     try:
         menu = int(input
             ("\n-------------------------------------------"
             "\n1. Sök efter en film baserat på exakt titel"
             "\n2. Sök efter en film baserat på något ord i filmens titel"
-            "\n3. Historik"
-            "\n4. Avsluta programmet."
+            "\n3. Senaste sökta film(erna)"
+            "\n4. Skriv ut sökordhistoriken"
+            "\n5. Avsluta programmet."
             "\n-------------------------------------------\n"))
         match menu:
             case 1: #Search for a movie with exact word
-                movie_title = input("Vilken film vill du söka efter?:\n")
+                movie_title = input("\nVilken film vill du söka efter?:\n")
+                with open("user_input.txt", "a", newline = "") as file:
+                    file.write(movie_title + "\n")
                 modules.Search_for_movie(movie_title)
             case 2: #Search for a movie with a word
-                modules.Search_for_movie_one_word(movie_title)
-                movie_title = input("Vilken film vill du söka efter?:\n")
-            case 3: #History
-                modules.history()
-            case 4: #Stäng av programmet
+                whole_movie_title = input("\nVilken film vill du söka efter?:\n")
+                with open("user_input.txt", "a", newline = "") as file:
+                    file.write(whole_movie_title + "\n")
+                modules.Search_for_movie_one_word(whole_movie_title)
+            case 3: #Last searched movie history
+                modules.Last_searched_movie()
+            case 4: #Userhistory
+                modules.Userhistory()
+            case 5: #Stäng av programmet
                 pass
             case default:
                 print("Ej giltig input, testa ett heltal.\n")
--- modules.py
+++ modules.py
@@ -4,62 +4,88 @@ import config
 
 API_KEY = config.api_key
 
+
 def Search_for_movie(movie_title):
-    url = f"http://www.omdbapi.com/?apikey={API_KEY}&t={movie_title}"
-    response = requests.get(url)
-    response.raise_for_status()
+    try:
+        url = f"http://www.omdbapi.com/?apikey={API_KEY}&t={movie_title}"
+        response = requests.get(url)
+        response.raise_for_status()
 
-    if response.status_code == 200:
-        print(response.status_code)
-        content_text = response.text
-        print(content_text)
-        movie_title = response.json().get('movies', [])
-        print(movie_title)
+        if response.status_code == 200:
+            print("\n", response.status_code)
+            movie_title = response.json()
 
-        with open('file.json', 'w', encoding='utf-8-sig') as fpointer:
+            for key in movie_title:
+                print("\n", key, ":", movie_title[key])
+            try:
+                with open('file.json', 'w', encoding = 'utf-8-sig') as fpointer:
                     json.dump(movie_title, fpointer, indent = 4, ensure_ascii = False)
+            except FileNotFoundError as err:
+                print("\n", err)
 
-    else:
-        print(f"Error: {movie_title.status_code}")
+        else:
+            print(f"Error: {movie_title.status_code}")
+    except requests.exceptions.HTTPError as ferr:
+        print("\nnot found")
+        print("\n", ferr)
 
-Search_for_movie("")
 
-def jprint(obj):
-    print(json.dumps(obj, sort_keys = True, indent = 4, ensure_ascii = False))
+def Search_for_movie_one_word(whole_movie_title):
+    try:
+        url = f"http://www.omdbapi.com/?apikey={API_KEY}&s={whole_movie_title}"
+        response = requests.get(url)
+        response.raise_for_status()
 
-def Search_for_movie_one_word(movie_title):
-    url = f"http://www.omdbapi.com/?apikey={API_KEY}&s={movie_title}"
-    response = requests.get(url)
-    response.raise_for_status()
+        if response.status_code == 200:
+            print("\n", response.status_code)
+            whole_movie_title = response.json()
 
-    if response.status_code == 200:
-        print(response.status_code)
-        content_text = response.text
-        print(content_text)
-        serched_movie = response.json().get('movies', [])
-        print(serched_movie)
+            for movie in whole_movie_title["Search"]:
+                for key, value in movie.items():
+                    print("\n", key, ":", value)
 
-        with open('file.json', 'w', encoding='utf-8-sig') as fpointer:
-                    json.dump(serched_movie, fpointer, indent = 4, ensure_ascii = False)
+            try:
+                with open('file.json', 'w', encoding='utf-8-sig') as fpointer:
+                    json.dump(whole_movie_title, fpointer, indent = 4, ensure_ascii = False)
+            except FileNotFoundError as err:
+                print(err)
 
-    else:
-        print(f"Error: {serched_movie.status_code}")
+        else:
+            print(f"Error: {whole_movie_title.status_code}")
+    except requests.exceptions.HTTPError as ferr:
+        print("\nnot found")
+        print("\n", ferr)
 
-Search_for_movie_one_word("")
 
-def jprint(obj):
-    print(json.dumps(obj, sort_keys = True, indent = 4, ensure_ascii = False))
-
-def history():
+def Last_searched_movie():
     while True:
         try:
-            with open("file.json", "r", encoding = "utf-8-sig") as jsonfile:
-                movies = json.load(jsonfile)
+            with open("file.json", "r", encoding = "utf-8-sig") as fpointer:
+                movies = json.load(fpointer)
 
-                for key in movies:
-                    print("\n", key, ":", movies[key])
+                if "Search" in movies:
+                    for movie in movies["Search"]:
+                        for key, value in movie.items():
+                            print("\n", key, ":", value)
+                else:
+                    for key in movies:
+                        print("\n", key, ":", movies[key])
                 print("\nAll data i JSON-filen är utskriven.")
                 break
+        except FileNotFoundError:
+            print("\nFilen kunde inte hittas.")
+            break
+
+
+def Userhistory():
+    while True:
+        try:
+            with open("user_input.txt", "r", encoding = "utf-8-sig") as file:
+                content = file.read()
+                print(content)
+
+            print("\nAlla sökord är utskrivna.")
+            break
         except FileNotFoundError:
             print("\nFilen kunde inte hittas.")
             break
